f79ae962b28bbaade5acf3a36e829fc5d9593361
CornYang <clay456789@gmail.com>
2023-07-18 00:53:47 +0800

    adjust program.cs & create startup.cs

--- Dotnet6WebProjectSetting.Sample.API/Program.cs
+++ Dotnet6WebProjectSetting.Sample.API/Program.cs
@@ -1,25 +1,10 @@
-var builder = WebApplication.CreateBuilder(args);
+using Dotnet6WebProjectSetting.Sample.API;
 
-// Add services to the container.
+var builder = Host.CreateDefaultBuilder(args);
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+builder.ConfigureWebHostDefaults(webBuilder =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    webBuilder.UseStartup<StartUp>();
+});
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+builder.Build().Run();
---
+++ Dotnet6WebProjectSetting.Sample.API/StartUp.cs
@@ -0,0 +1,37 @@
+﻿namespace Dotnet6WebProjectSetting.Sample.API
+{
+    public class StartUp
+    {
+        public StartUp(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
