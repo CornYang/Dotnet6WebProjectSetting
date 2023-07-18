154fa3051cefe46ab597146542c47e864e6f6599
CornYang <clay456789@gmail.com>
2023-07-19 01:43:22 +0800

    add NLog & create a Test HomeController & add condition to .gitignore

---
+++ Dotnet6WebProjectSetting.Sample.API/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLog;
+
+namespace Dotnet6WebProjectSetting.Sample.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
+        public HomeController()
+        {
+
+        }
+
+        public string Get()
+        {
+            _logger.Info($"Helloworld");
+            return "Helloworld";
+        }
+    }
+}
--- Dotnet6WebProjectSetting.Sample.API/Program.cs
+++ Dotnet6WebProjectSetting.Sample.API/Program.cs
@@ -1,10 +1,15 @@
 using Dotnet6WebProjectSetting.Sample.API;
+using NLog;
+using NLog.Web;
 
+// Early init of NLog to allow startup and exception logging, before host is built
+//reference: https://github.com/NLog/NLog/wiki/Getting-started-with-ASP.NET-Core-6
+NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
 var builder = Host.CreateDefaultBuilder(args);
 
 builder.ConfigureWebHostDefaults(webBuilder =>
 {
     webBuilder.UseStartup<StartUp>();
-});
+}).UseNLog(); //µù¥UNLog(DI)
 
 builder.Build().Run();
