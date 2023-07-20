d72cd331967522e14dc88b6d052dcfbb1421b03c
CornYang <clay456789@gmail.com>
2023-07-21 01:22:29 +0800

    add exceptionMiddleware

---
+++ Dotnet6WebProjectSetting.Sample.API/Helper/ExceptionMiddleware.cs
@@ -0,0 +1,26 @@
+﻿namespace Dotnet6WebProjectSetting.Sample.API.Helper
+{
+    //reference : https://eugenesu.me/2021/09/09/dotnetcore-global-error-handler/
+    //reference : https://blog.johnwu.cc/article/ironman-day17-asp-net-core-exception-handler.html
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
--- Dotnet6WebProjectSetting.Sample.API/StartUp.cs
+++ Dotnet6WebProjectSetting.Sample.API/StartUp.cs
@@ -1,4 +1,6 @@
-﻿namespace Dotnet6WebProjectSetting.Sample.API
+﻿using Dotnet6WebProjectSetting.Sample.API.Helper;
+
+namespace Dotnet6WebProjectSetting.Sample.API
 {
     public class StartUp
     {
@@ -22,6 +24,11 @@
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            //Middleware 註冊的層級可以在 Filters 的外層，也就是說所有的 Filter 都會經過 Middleware。
+            //如果再把 Exception Middleware 註冊在所有 Middleware 的最外層，就可以變成全站的 Exception Handler。
+            //Middleware 的註冊順序很重要，越先註冊的會包在越外層。把 ExceptionMiddleware 註冊在越外層，能涵蓋的範圍就越多。
+            app.UseMiddleware<ExceptionMiddleware>();
 
             app.UseRouting();
 
