aa8d901153025523853e83e036dcb16163aedd9a
CornYang <clay456789@gmail.com>
2023-07-22 11:02:19 +0800

    Create DI Services Extenison & Add DI Services to StartUp

---
+++ Dotnet6WebProjectSetting.Sample.API/Helper/DependencyInjectionServices.cs
@@ -0,0 +1,11 @@
+﻿namespace Dotnet6WebProjectSetting.Sample.API.Helper
+{
+    public static class DependencyInjectionServices
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+
+            return services;
+        }
+    }
+}
--- Dotnet6WebProjectSetting.Sample.API/Program.cs
+++ Dotnet6WebProjectSetting.Sample.API/Program.cs
@@ -3,7 +3,7 @@ using NLog;
 using NLog.Web;
 
 // Early init of NLog to allow startup and exception logging, before host is built
-//reference: https://github.com/NLog/NLog/wiki/Getting-started-with-ASP.NET-Core-6
+// reference: https://github.com/NLog/NLog/wiki/Getting-started-with-ASP.NET-Core-6
 NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
 var builder = Host.CreateDefaultBuilder(args);
 
